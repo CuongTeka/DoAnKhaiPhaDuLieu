edb1ab5bbd776bd794e9ea52e5d57ba6df5c9248
Teka <cuongteka2711@gmail.com>
2026-04-21 12:24:53 +0700

    remove img

--- src/assets/number/number.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import zero from "./0.png";
-import one from "./1.png";
-import two from "./2.png";
-import three from "./3.png";
-
-const number = [zero, one, two, three];
-
-export default number;
--- src/assets/star/star.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import st from "./star.png";
-import stbg from "./star_bg.png";
-
-const star = () => {
-  return [st, stbg];
-};
-
-export default star;
